3a2b0146a99d0f34a1e58fcdc7b1cf1313d4553f
Felipe Mayworm <5205476+felipemayworm@users.noreply.github.com>
2017-11-20 17:52:13 +0000

    add files

---
+++ java/src/main/java/com/sap/employeeslist/EmployeesListServiceFactory.java
@@ -0,0 +1,26 @@
+
+package com.sap.employeeslist;
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.olingo.odata2.jpa.processor.api.ODataJPAContext;
+import org.apache.olingo.odata2.jpa.processor.api.ODataJPAServiceFactory;
+import org.apache.olingo.odata2.jpa.processor.api.exception.ODataJPARuntimeException;
+
+public class EmployeesListServiceFactory extends ODataJPAServiceFactory {
+    private static final String PERSISTENCE_UNIT_NAME = "employeeslist";
+    
+    @Override
+    public ODataJPAContext initializeODataJPAContext()
+            throws ODataJPARuntimeException {
+        ODataJPAContext oDataJPAContext = this.getODataJPAContext();
+        try {
+        	EntityManagerFactory emf = JpaEntityManagerFactory.getEntityManagerFactory();
+			oDataJPAContext.setEntityManagerFactory(emf);
+            oDataJPAContext.setPersistenceUnitName(PERSISTENCE_UNIT_NAME);
+            return oDataJPAContext;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
